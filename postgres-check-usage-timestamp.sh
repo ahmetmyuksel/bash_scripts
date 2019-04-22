d1afcce62c462f55a0db87062604ba7b5a7008ce
Ahmet M. Yüksel <mchtyksl@gmail.com>
2019-04-22 16:20:38 +0300

    Postgres - Connecting postgresql and checks a table. If there is any input with status 20 in last 20 mins creates alarm

---
+++ postgres-check-usage-timestamp.sh
@@ -0,0 +1,16 @@
+hostname=$(postgres_host)
+port=$(postgres_port)
+user=$(postgres_user)
+password=$(postgres_password)
+database=$(postgres_database)
+table=$(postgres_table)
+
+value=$(/opt/sensu/embedded/bin/ruby /opt/sensu/embedded/bin/metrics-postgres-query.rb -h $postgres_host -u $postgres_user -P $postgres_port -p $postgres_password -d $postgres_database -q "select count(*) from $postgres_table where created_at < NOW() - INTERVAL '20 minutes' and status = 20;" | awk '{print $2}')
+
+if [[ $value -ne 0 ]] ; then
+  echo 'WARNING:' $postgres_table 'table has a record as status=20 in last 20 minutes'
+  echo 'Row count=' $value
+  exit 2
+fi
+echo 'OK: Timestamp and status values are appropriate.'
+exit 0
