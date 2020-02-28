be82160e442054aea562ca7b06cc00d35e12b74b
Ahmet M. Yüksel <35404993+ahmetyuksel@users.noreply.github.com>
2020-02-28 11:28:19 +0300

    Update openshift_and_registry_login.sh

--- scripts/openshift_and_registry_login.sh
+++ scripts/openshift_and_registry_login.sh
@@ -1,17 +1,30 @@
+#!/usr/bin/env bash
 #token=$(ssh root@10.10.10.10 'oc login -u admin -p q1w2e3r4' )
-/usr/local/bin/oc login https://ocp.mydomain.com:8443 --insecure-skip-tls-verify=true -u admin -p password
+#/usr/local/bin/oc login https://ocp.mydomain.com:8443 --insecure-skip-tls-verify=true -u admin -p password
 #ssh root@10.10.10.10 'oc login https://ocp.mydomain.com:8443 --insecure-skip-tls-verify=true -u admin -p password'
 
-if [[ ! $? -eq 0 ]]; then
-        echo CRITICAL: can not login to openshift
-            exit 2
+LOCATION=$1
+if [[ $(echo $LOCATION) == "openshift.location1" ]]; then
+  token=$(ssh root@10.10.10.10 'oc login -u admin -p password1' )
+elif [[ $(echo $LOCATION) == "openshift.location2" ]]; then
+  token=$(ssh root@192.168.10.10 'oc login -u admin -p password2' )
+else
+  exit 2
 fi
 
-token=$(ssh root@10.10.10.10 'oc whoami --show-token')
-echo $token
-docker login -p $token -e unused -u unused docker-registry-default.apps.skyz.tech
-echo docker login -p $token -e unused -u unused docker-registry-default.apps.skyz.tech
 if [[ ! $? -eq 0 ]]; then
-        echo CRITICAL: can not login to openshift registry
-            exit 2
+    echo CRITICAL: can not login to openshift
+    exit 2
+fi
+
+if [[ $(echo $LOCATION) == "openshift.location1" ]]; then
+  token=$(ssh root@10.10.10.10 'oc whoami --show-token')
+  docker login -p $token -u unused docker-registry-default.apps.location1
+elif [[ $(echo $LOCATION) == "openshift.location2" ]]; then
+  token=$(ssh root@192.168.10.10 'oc whoami --show-token')
+  docker login -p $token -u unused docker-registry-default.apps.location2
+fi
+if [[ ! $? -eq 0 ]]; then
+    echo CRITICAL: can not login to openshift
+    exit 2
 fi
