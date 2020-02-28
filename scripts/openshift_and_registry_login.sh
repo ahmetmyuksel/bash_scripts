4a4bd0f26dc1cad92b785b3ce4b5724a17936ac9
Ahmet M. Yüksel <35404993+ahmetyuksel@users.noreply.github.com>
2020-02-28 11:20:34 +0300

    Update openshift_and_registry_login.sh

--- scripts/openshift_and_registry_login.sh
+++ scripts/openshift_and_registry_login.sh
@@ -1,12 +1,17 @@
-#ssh root@10.10.10.10 'oc login https://openshift.domainname.com:8443 --insecure-skip-tls-verify=true -u admin -p changeme'
+#token=$(ssh root@10.10.10.10 'oc login -u admin -p q1w2e3r4' )
+/usr/local/bin/oc login https://ocp.mydomain.com:8443 --insecure-skip-tls-verify=true -u admin -p password
+#ssh root@10.10.10.10 'oc login https://ocp.mydomain.com:8443 --insecure-skip-tls-verify=true -u admin -p password'
+
+if [[ ! $? -eq 0 ]]; then
+        echo CRITICAL: can not login to openshift
+            exit 2
+fi
 
 token=$(ssh root@10.10.10.10 'oc whoami --show-token')
 echo $token
-
-docker login -p $token -u unused docker-registry-default.apps.domainname.com
-echo 'docker login -p' $token '-u unused docker-registry-default.apps.domainname.com'
-
+docker login -p $token -e unused -u unused docker-registry-default.apps.skyz.tech
+echo docker login -p $token -e unused -u unused docker-registry-default.apps.skyz.tech
 if [[ ! $? -eq 0 ]]; then
-    echo CRITICAL: can not login to openshift registry
-    exit 2
+        echo CRITICAL: can not login to openshift registry
+            exit 2
 fi
